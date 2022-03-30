bdc5e1fbafbd93ae424eef5a0982a850e9f564c8
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-03-31 01:46:51 +0200

    add confetti pckg and set it up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Die from "./components/Die";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 export default function App() {
   const [dice, setDice] = React.useState(allNewDice());
@@ -67,6 +68,7 @@ export default function App() {
 
   return (
     <div className="container">
+      {tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its
