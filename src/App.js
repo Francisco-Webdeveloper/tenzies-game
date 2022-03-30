50bc8c0692ccea7f863d0f8f8570bc8f020f9683
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-03-30 22:09:50 +0200

    render react elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import React from "react";
+import Die from "./components/Die";
 
 export default function App() {
-  return <div className="container"></div>;
+  const [dice, setDice] = React.useState(randomDice());
+
+  function randomDice() {
+    const arrayOfDice = [];
+    for (let i = 0; i < 10; i++) {
+      const randomDie = Math.ceil(Math.random() * 6);
+      arrayOfDice.push(randomDie);
+    }
+    return arrayOfDice;
+  }
+
+  console.log(dice);
+
+  return (
+    <div className="container">
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <Die dice={dice} />
+      <button>Roll</button>
+    </div>
+  );
 }
