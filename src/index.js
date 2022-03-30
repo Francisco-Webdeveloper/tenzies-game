ac8eb1077ce5b2e818a152c4a4ab74fe90537d15
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-03-30 22:09:08 +0200

    update render method

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import * as ReactDOMClient from "react-dom/client";
 import "./style.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOMClient.createRoot(document.getElementById("root")).render(<App />);
